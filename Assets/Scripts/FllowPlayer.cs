7e44171a00c872b68c1c170126d6b8f6a5ea8954
Adem Ertaş <adem_ertas@aof.anadolu.edu.tr>
2022-04-18 23:07:00 +0300

    camera ayarları/ zombıe yonu/ NavMash ayarları yapıldı

--- Assets/Scripts/FllowPlayer.cs
+++ Assets/Scripts/FllowPlayer.cs
@@ -1,24 +1,36 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class FllowPlayer : MonoBehaviour
 {
-     private GameObject _player;
-     [SerializeField] private Animator _zombieAnimator;
+    #region NavMash'den oncesi
+    //  private GameObject _player;
+    //  [SerializeField] private Animator _zombieAnimator;
+    //
+    //  private void Start()
+    //  {
+    //      _player = FindObjectOfType<PlayerController>().gameObject;
+    //  }
+    //  private void Update()
+    // {
+    //     transform.position =Vector3.MoveTowards(this.transform.position, _player.transform.position, 1f * Time.deltaTime);
+    //     Vector3 relativePos = _player.transform.position - transform.position;
+    //     transform.rotation = Quaternion.LookRotation(relativePos, Vector3.up);
+    // }
+     #endregion
 
-     private void Start()
+     [SerializeField] private Transform _player;
+     private NavMeshAgent _nMash;
+
+     private void Awake()
      {
-         _player = FindObjectOfType<PlayerController>().gameObject;
+         _player = FindObjectOfType<PlayerController>().transform;
+         _nMash = GetComponent<NavMeshAgent>();
      }
 
      private void Update()
-    {
-        transform.position =
-            Vector3.MoveTowards(this.transform.position, _player.transform.position, 1f * Time.deltaTime);
-
-        Vector3 relativePos = _player.transform.position - transform.position;
-        transform.rotation = Quaternion.LookRotation(relativePos, Vector3.up);
-    }
+     {
+         _nMash.SetDestination(_player.position);
+     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,14 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _speed;
     [SerializeField] Animator _playerAnim;
+    private Transform _cameraTransform;
+    [SerializeField] private float rotationSpeed;
+
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked; // oyun oynarken ımlecı orataya sabıtlıyor gozukmuyor..
+        _cameraTransform = Camera.main.transform; 
+    }
 
     private void Update()
     {
@@ -15,10 +23,14 @@ public class PlayerController : MonoBehaviour
 
     void Move()
     {
-        transform.position += new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * _speed, 0,
-            Input.GetAxis("Vertical") * Time.deltaTime * _speed);
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * _speed, 0,
+            Input.GetAxis("Vertical") * Time.deltaTime * _speed)); 
+        
         _playerAnim.SetFloat("MoveX", Input.GetAxis("Horizontal"));
         _playerAnim.SetFloat("MoveZ", Input.GetAxis("Vertical"));
+        
+        Quaternion targetRotation = Quaternion.Euler(0,_cameraTransform.eulerAngles.y,0);
+        transform.rotation = Quaternion.Lerp(transform.rotation,targetRotation,rotationSpeed * Time.deltaTime);
     }
 
 }
